ee5dfc32b829ed4355c5e9f3c0a7b325b5eff383
MailaMorais <mailla.info2b@gmail.com>
2016-12-09 08:08:00 -0400

    teste

--- application/views/listarProdutos.php
+++ application/views/listarProdutos.php
@@ -1,4 +1,15 @@
 <?php
+defined('BASEPATH') OR exit ('No direct script access allowed');
+?>
 
+<!DOCTYPE html>
 
+<html lang="pt-BR">
+    <head>
+        <meta charset="utf-8">
+        <title>Lista de Produtos</title>
+    </head>
+    
+    teste
+</html>
 
